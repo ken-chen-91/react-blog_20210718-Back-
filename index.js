71062ca385f81f34167d4864d1e99758621811c7
kenchen <a330120231@gmail.com>
2021-07-21 23:59:24 -0700

    ...

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+// const multer = require("multer");
+// const path = require("path");
+
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+
+dotenv.config();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}).then(console.log("Connected to MongoDB")).catch(err => console.log(err))
+
+app.use("/api/auth", authRoute);
+app.use("/api/users",userRoute)
+
+app.listen("5000", () => {
+    console.log("Backend is running")
+})
+
+
+
+
+
+
+
+
+
+
+
+
